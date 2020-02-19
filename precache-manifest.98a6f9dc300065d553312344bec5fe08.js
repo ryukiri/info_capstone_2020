bafac025012379e93d5bc3b7916910e6bf5a2779
Austin Quach <austinquachh@gmail.com>
2020-02-18 20:11:15 -0800

    Updates

---
+++ precache-manifest.98a6f9dc300065d553312344bec5fe08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2001aca3a5893236898b002a14402757",
+    "url": "/info_capstone_2020/index.html"
+  },
+  {
+    "revision": "febf8f41f7fc0beab255",
+    "url": "/info_capstone_2020/static/css/main.d734c80a.chunk.css"
+  },
+  {
+    "revision": "c355c107fb370c334def",
+    "url": "/info_capstone_2020/static/js/2.d533c0c1.chunk.js"
+  },
+  {
+    "revision": "4a2932a9a7e9b3a27604858fef2249e4",
+    "url": "/info_capstone_2020/static/js/2.d533c0c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "febf8f41f7fc0beab255",
+    "url": "/info_capstone_2020/static/js/main.d0397f21.chunk.js"
+  },
+  {
+    "revision": "e2c5335c4cfe02a89cc9",
+    "url": "/info_capstone_2020/static/js/runtime-main.bb692b19.js"
+  },
+  {
+    "revision": "0578390c3b8e628b67718acc5d498e63",
+    "url": "/info_capstone_2020/static/media/bus.0578390c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/info_capstone_2020/precache-manifest.512768aeb75a3f087ff030753cd73d08.js"
+  "/info_capstone_2020/precache-manifest.98a6f9dc300065d553312344bec5fe08.js"
 );
 
 self.addEventListener('message', (event) => {
