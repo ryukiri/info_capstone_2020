b754848e75d5940c3b1d8985dd7defca9de574bd
Austin Quach <austinquachh@gmail.com>
2020-02-20 11:43:32 -0800

    Drawer & fix login bug

--- src/components/ButtonAppBar/ButtonAppBarSignOut.js
+++ src/components/ButtonAppBar/ButtonAppBarSignOut.js
@@ -4,7 +4,15 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import Grid from "@material-ui/core/Grid";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MailIcon from "@material-ui/icons/Mail";
 import "./ButtonAppBar.css";
 import { Link } from "react-router-dom";
 import app from "./../firebase/base";
@@ -29,10 +37,87 @@ const useStyles = makeStyles(theme => ({
 export default function ButtonAppBar() {
   const classes = useStyles();
 
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
+
+  const toggleDrawer = (side, open) => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        <Link to="/overview" className={"noDecorations"}>
+          <ListItem button key={"Overview"}>
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Overview"} />
+          </ListItem>
+        </Link>
+        <ListItem button key={"Media"}>
+          <ListItemIcon>
+            <MailIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Media"} />
+        </ListItem>
+        <ListItem button key={"Health"}>
+          <ListItemIcon>
+            <MailIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Health"} />
+        </ListItem>
+        <ListItem button key={"Quiz"}>
+          <ListItemIcon>
+            <MailIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Quiz"} />
+        </ListItem>
+        <ListItem button key={"Food"}>
+          <ListItemIcon>
+            <MailIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Food"} />
+        </ListItem>
+        <ListItem button key={"Leaderboard"}>
+          <ListItemIcon>
+            <MailIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Leaderboard"} />
+        </ListItem>
+      </List>
+    </div>
+  );
+
   return (
     <div className={classes.root}>
       <AppBar position="static" color="white">
         <Toolbar className={classes.menuBackgroundColor}>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer("left", true)}
+          >
+            <MenuIcon />
+          </IconButton>
           <Grid justify="space-between" container spacing={24}>
             <Grid item>
               <Button color="white">
@@ -42,6 +127,9 @@ export default function ButtonAppBar() {
                   </Link>
                 </Typography>
               </Button>
+              <Drawer open={state.left} onClose={toggleDrawer("left", false)}>
+                {sideList("left")}
+              </Drawer>
             </Grid>
             <Grid item>
               <div>
--- src/components/firebase/auth.js
+++ src/components/firebase/auth.js
@@ -4,10 +4,14 @@ import app from './base.js';
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-	const [currentUser, setCurrentUser] = useState(null);
+	const [currentUser, setCurrentUser] = useState([], () => {
+		const localData = localStorage.getItem('userData');
+		return localData ? JSON.parse(localData) : [];
+	});
 
 	useEffect(() => {
 		app.auth().onAuthStateChanged(setCurrentUser);
+		localStorage.setItem('userData', JSON.stringify(currentUser));
 	}, []);
 
 	return (
