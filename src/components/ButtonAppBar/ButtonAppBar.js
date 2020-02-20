f2e63a7adc9fa6a91c1563cef350b36c740c7b54
Austin Quach <austinquachh@gmail.com>
2020-02-19 22:51:20 -0800

    Fix formatting

--- src/components/ButtonAppBar/ButtonAppBar.js
+++ src/components/ButtonAppBar/ButtonAppBar.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import './ButtonAppBar.css';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import "./ButtonAppBar.css";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: '#B4DFE5'
+    backgroundColor: "#B4DFE5"
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
     flexGrow: 1,
-    color: '#F4976C',
+    color: "#F4976C"
   },
   menuBackgroundColor: {
-    backgroundColor: '#B4DFE5'
+    backgroundColor: "#B4DFE5"
   }
 }));
 
@@ -32,29 +32,31 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static" color="white">
         <Toolbar className={classes.menuBackgroundColor}>
-          <Grid
-            justify="space-between"
-            container 
-            spacing={24}
-          >
+          <Grid justify="space-between" container spacing={24}>
             <Grid item>
               <Button color="white">
                 <Typography variant="h6" className={classes.title}>
-                  <Link to="/" className={"noDecorations"}>Data Diary</Link>
+                  <Link to="/" className={"noDecorations"}>
+                    Data Diary
+                  </Link>
                 </Typography>
               </Button>
             </Grid>
             <Grid item>
               <div>
-                <Button color="white" style={{float: "right"}}>
+                <Button color="white" style={{ float: "right" }}>
                   <Typography variant="body1" gutterBottom>
-                    <Link to="/login" className={"noDecorations"}>Login</Link>
+                    <Link to="/login" className={"noDecorations"}>
+                      Login
+                    </Link>
                   </Typography>
                 </Button>
-                
-                <Button color="white" style={{float: "right"}}>
+
+                <Button color="white" style={{ float: "right" }}>
                   <Typography variant="body1" gutterBottom>
-                    <Link to="/about" className={"noDecorations"}>About</Link>
+                    <Link to="/about" className={"noDecorations"}>
+                      About
+                    </Link>
                   </Typography>
                 </Button>
               </div>
@@ -62,7 +64,6 @@ export default function ButtonAppBar() {
           </Grid>
         </Toolbar>
       </AppBar>
-
     </div>
   );
 }
