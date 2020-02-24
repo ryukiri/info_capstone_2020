0855a5b2c3b2a5f6542d5d60ebcf8376cc565fbc
Austin Quach <austinquachh@gmail.com>
2020-02-23 18:39:30 -0800

    Updates

---
+++ precache-manifest.e28076a4debcf5759d0d891647e5d008.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02de13ed3d8f5a727fcda13d7ad64fbf",
+    "url": "/info_capstone_2020/index.html"
+  },
+  {
+    "revision": "67732eb0c3243c7195bd",
+    "url": "/info_capstone_2020/static/css/main.b2ff8e0f.chunk.css"
+  },
+  {
+    "revision": "25afefa7f400c63b09f4",
+    "url": "/info_capstone_2020/static/js/2.6e87adac.chunk.js"
+  },
+  {
+    "revision": "d3cacbbf0d4cb6556a1a4a364e240eda",
+    "url": "/info_capstone_2020/static/js/2.6e87adac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67732eb0c3243c7195bd",
+    "url": "/info_capstone_2020/static/js/main.885675a6.chunk.js"
+  },
+  {
+    "revision": "e2c5335c4cfe02a89cc9",
+    "url": "/info_capstone_2020/static/js/runtime-main.bb692b19.js"
+  },
+  {
+    "revision": "0578390c3b8e628b67718acc5d498e63",
+    "url": "/info_capstone_2020/static/media/bus.0578390c.jpg"
+  },
+  {
+    "revision": "2464bafa05b60f6b9d1ed01013e2f170",
+    "url": "/info_capstone_2020/static/media/diary.2464bafa.jpg"
+  },
+  {
+    "revision": "72aca40f87abd302b82f4f0e53d7aa63",
+    "url": "/info_capstone_2020/static/media/diary2.72aca40f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/info_capstone_2020/precache-manifest.44103cd7e953c64f895e26611e402506.js"
+  "/info_capstone_2020/precache-manifest.e28076a4debcf5759d0d891647e5d008.js"
 );
 
 self.addEventListener('message', (event) => {
