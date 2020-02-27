87e6a1b471d23116ee0db9814a36eea38c1550a8
Austin Quach <austinquachh@gmail.com>
2020-02-26 19:45:04 -0800

    Add react-swipeable-views and tabs to leaderboard page

--- src/pages/leaderboard/Leaderboard.js
+++ src/pages/leaderboard/Leaderboard.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import Box from "@material-ui/core/Box";
 import ButtonAppBar from "../../components/ButtonAppBar/ButtonAppBarSignOut";
 import { Container, Grid } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
-import { makeStyles, withTheme } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import red from "@material-ui/core/colors/red";
@@ -11,6 +14,36 @@ import purple from "@material-ui/core/colors/purple";
 import blue from "@material-ui/core/colors/blue";
 import teal from "@material-ui/core/colors/teal";
 import "./Leaderboard.css";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`
+  };
+}
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -56,9 +89,14 @@ const useStyles = makeStyles(theme => ({
 function Leaderboard(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const theme = useTheme();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+
+  const handleChangeIndex = index => {
+    setValue(index);
   };
 
   return (
@@ -67,35 +105,53 @@ function Leaderboard(props) {
       <Container className={classes.container}>
         <Typography variant="h3">Leaderboard</Typography>
 
-        <Tabs
-          className={classes.paddingBottom}
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
-        >
-          <Tab label="Friends" />
-          <Tab label="All" />
-          <Tab label="School" />
-          <Tab label="Grade" />
-        </Tabs>
+        <div className={classes.root}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+          >
+            <Tab label="Friends" {...a11yProps(0)} />
+            <Tab label="School" {...a11yProps(1)} />
+            <Tab label="Grade" {...a11yProps(2)} />
+            <Tab label="All" {...a11yProps(3)} />
+          </Tabs>
+          <SwipeableViews
+            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              <Container className={classes.paddingBottom}>
+                <Paper className={classes.paperRed}>xs=12</Paper>
+              </Container>
 
-        <Container className={classes.paddingBottom}>
-          <Paper className={classes.paperRed}>xs=12</Paper>
-        </Container>
+              <Container className={classes.paddingBottom}>
+                <Paper className={classes.paperPurple}>xs=12</Paper>
+              </Container>
 
-        <Container className={classes.paddingBottom}>
-          <Paper className={classes.paperPurple}>xs=12</Paper>
-        </Container>
+              <Container className={classes.paddingBottom}>
+                <Paper className={classes.paperBlue}>xs=12</Paper>
+              </Container>
 
-        <Container className={classes.paddingBottom}>
-          <Paper className={classes.paperBlue}>xs=12</Paper>
-        </Container>
-
-        <Container className={classes.paddingBottom}>
-          <Paper className={classes.paperTeal}>xs=12</Paper>
-        </Container>
+              <Container className={classes.paddingBottom}>
+                <Paper className={classes.paperTeal}>xs=12</Paper>
+              </Container>
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+              School
+            </TabPanel>
+            <TabPanel value={value} index={2} dir={theme.direction}>
+              Grade
+            </TabPanel>
+            <TabPanel value={value} index={3} dir={theme.direction}>
+              All
+            </TabPanel>
+          </SwipeableViews>
+        </div>
       </Container>
     </div>
   );
