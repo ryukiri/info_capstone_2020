651d9d4396df36097a372c1055aa6d617ffe7044
Ian Callender <iancalle@uw.edu>
2020-03-12 16:16:47 -0700

    summary page graphs added

--- src/pages/summary/Summary.js
+++ src/pages/summary/Summary.js
@@ -3,19 +3,36 @@ import Typography from "@material-ui/core/Typography";
 import ButtonAppBar from "../../components/ButtonAppBar/ButtonAppBarSignOut";
 import app from "./../../components/firebase/base";
 import "./Summary.css";
+import { VictoryChart, VictoryBar, VictoryTheme, VictoryLine, VictoryScatter } from "victory";
+import Grid from "@material-ui/core/Grid";
 
 class Summary extends Component {
   constructor(props) {
+    var graphData = {}
+    app.database().ref("diaryEntries/" + "nfESSRX4ByNjD7DPeUs2UysN9vD3").once('value').then(function(snapshot) {
+      snapshot.forEach((child) => {
+        child.forEach((question) => {
+          if(graphData[question.key.toString()] != null) {
+            graphData[question.key.toString()].push({x: child.key, y: parseInt(question.val())})
+          } else {
+            graphData[question.key.toString()] = [{x: child.key, y: parseInt(question.val())}]
+          }
+        });
+      });
+    });
+    
     super(props);
-    this.state = { messages: [] }; // <- set up react state
+    this.state = { messages: [], graphData: graphData}; // <- set up react state
   }
+
   componentDidMount() {
+
     /* Create reference to messages in Firebase Database */
     let messagesRef = app
       .database()
       .ref("diary")
       .orderByKey()
-      .limitToLast(100);
+      .limitToLast(20);
 
     /* Update React state when message is added at Firebase Database */
     messagesRef.on("child_added", snapshot => {
@@ -24,23 +41,59 @@ class Summary extends Component {
         sleep: snapshot.val().sleep,
         date: snapshot.key
       };
-      this.setState({ messages: [diary].concat(this.state.messages) });
+      let data = {
+        "x": snapshot.key,
+        "y": snapshot.val().banana
+      };
+      this.setState({ messages: [diary].concat(this.state.messages)});
     });
+
+    var graphData = {}
+    app.database().ref("diaryEntries/" + "nfESSRX4ByNjD7DPeUs2UysN9vD3").on('value', function(snapshot) {
+      snapshot.forEach((child) => {
+        child.forEach((question) => {
+          if(graphData[question.key.toString()] != null) {
+            graphData[question.key.toString()].push({x: child.key, y: parseInt(question.val())})
+          } else {
+            graphData[question.key.toString()] = [{x: child.key, y: parseInt(question.val())}]
+          }
+        });
+      });
+    });
+    this.setState({ graphData: graphData});
   }
 
   render() {
+
+    function getChart(data) {
+      let chart;
+      var randomNum = Math.floor(Math.random() * 3); 
+      if (randomNum == 0) {
+        chart = <VictoryLine data={data}/>
+      } else if (randomNum == 1) {
+        chart = <VictoryScatter style={{ data: { fill: "#c43a31" } }} size={7} data={data}/>
+      } else {
+        chart = <VictoryBar style={{ data: { fill: "#c43a31" } }} alignment="start" data={data}/>
+      }
+      return chart;
+    }
+
     return (
       <div>
         <ButtonAppBar />
-        <ul>
-          {/* Render the list of messages */
-          this.state.messages.map(message => (
-            <li key={message.date}>
-              Today is {message.date}. You ate {message.banana} bananas and
-              slept {message.sleep} hours.
-            </li>
-          ))}
-        </ul>
+
+        <Grid container spacing={4}>
+          {
+            Object.keys(this.state.graphData).map(key => (
+              <Grid item xs={12} sm={12} md={6} lg={6} xl={6} align="center"> 
+                <VictoryChart theme={VictoryTheme.material}>
+                  {getChart(this.state.graphData[key])}
+                </VictoryChart>
+              </Grid>
+            ))
+          }
+        </Grid>
+
       </div>
     );
   }
