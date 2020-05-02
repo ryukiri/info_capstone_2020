10330fbab1bd0f9b82d707aa4eb19741566c7544
Ian Callender <iancalle@uw.edu>
2020-05-01 23:08:10 -0700

    working on merigng branches

---
+++ src/components/Graphs/Graphs.js
@@ -0,0 +1,141 @@
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { VictoryChart, VictoryBar, VictoryTheme, VictoryLine, VictoryScatter, VictoryAxis } from "victory";
+import "./Graphs.css";
+import app from "../../components/firebase/base";
+
+class Graphs extends Component {
+
+    constructor(props) { //the constructor must take a `props` parameter
+        super(props)      //the constructor must call superclass constructor
+
+        this.state = {
+          titles: {},
+          graphData: {},
+          dataCategory:[],
+          isLoadingGraph: false,
+        }
+    }
+
+    componentDidMount() {
+      this.authUser().then(
+        (user) => {
+          var dataCategory = []
+          var graphData = {}
+          var titles = {}
+
+          app.database().ref("users/" + this.getCurrentUser() + "/diaryQuestionCategories").once("value", (snapshot) => {
+              snapshot.forEach((child) => {
+                  dataCategory.push(child.val())
+              });
+          });
+  
+          app.database().ref("users/" + this.getCurrentUser() + "/diaryQuestions").once("value", (snapshot) => {
+            snapshot.forEach((child) => {
+              titles[child.key.toString()] = child.val()
+            });
+          });
+  
+          app.database().ref("diaryEntries/" + this.getCurrentUser()).limitToLast(5).once("value", (snapshot) => {
+            snapshot.forEach((child) => {
+                  child.forEach((question) => {
+                    if(graphData[question.key.toString()] != null) {
+                      graphData[question.key.toString()].push({x: (child.key.substring(0,2) + "/" + child.key.substring(2,4)), y: parseInt(question.val())})
+                    } else {
+                      graphData[question.key.toString()] = [{x: (child.key.substring(0,2) + "/" + child.key.substring(2,4)), y: parseInt(question.val())}]
+                    }
+                  });
+            });
+            this.setState({
+              titles: titles,
+              graphData: graphData,
+              dataCategory: dataCategory,
+              isLoadingGraph: true,
+            })
+          });
+        },
+        (error) => {
+          alert(error);
+        }
+      ); 
+    }
+
+      getCurrentUser() {
+        var user = app.auth().currentUser;
+    
+        if (user) {
+          return user.uid;
+        } else {
+          // No user is signed in.
+        }
+      }
+
+      authUser() {
+        return new Promise(function (resolve, reject) {
+          app.auth().onAuthStateChanged(function (user) {
+            if (user) {
+              resolve(user);
+            } else {
+              reject("User not logged in");
+            }
+          });
+        });
+      }
+
+      render() {
+
+        var newTitles = { ...this.state.titles };
+        var oldGraphData = { ...this.state.graphData };
+        var categories = this.state.dataCategory
+        if(this.state.graphData != null) {
+          for (let i = 0; i < 5; i++) {
+            if(categories[i] != this.props.category && this.props.category != "") {
+              delete oldGraphData[('q' + (i + 1))]
+              delete newTitles[('q' + (i + 1))]
+            }
+          }
+        }
+
+        function getChart(data) {
+            let chart;
+            var randomNum = Math.floor(Math.random() * 3); 
+            if (randomNum == 0) {
+              chart = <VictoryLine data={data}/>
+            } else if (randomNum == 1) {
+              chart = <VictoryScatter style={{ data: { fill: "#c43a31" } }} size={7} data={data}/>
+            } else {
+              chart = <VictoryBar style={{ data: { fill: "#c43a31" }, labels: { padding: 100 } }} alignment="start" data={data}/>
+            }
+            return chart;
+          }
+
+        return (
+          this.state.isLoadingGraph && (<div>
+              <Typography variant="h3" component="h4" className="titleText" style={{padding: "10px", color: "primary", fontWeight: "bold"}}>
+                  {this.props.category}
+              </Typography>
+              <Grid container spacing={4}>
+              {
+                  Object.keys(oldGraphData).map(key => (
+                  <Grid item xs={12} sm={12} md={6} lg={6} xl={6} align="center"> 
+                      <Typography variant="h4">{newTitles[key]}</Typography>
+                      <VictoryChart theme={VictoryTheme.material}>
+                      <VictoryAxis
+                          style={{axisLabel: {padding: 30}}}
+                          label="Date"
+                      />
+                      <VictoryAxis dependentAxis/>
+                      {getChart(oldGraphData[key])}
+                      </VictoryChart>
+                  </Grid>
+                  ))
+              }
+              </Grid>
+            </div>)
+        )
+      }
+
+}
+
+export default Graphs;
