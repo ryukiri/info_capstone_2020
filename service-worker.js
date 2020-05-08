513d9306cadd72350e1511b409c7ee03f4eac41a
Austin Quach <austinquachh@gmail.com>
2020-05-08 00:23:46 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/info_capstone_2020/precache-manifest.57703bb5c7a6d0077fc1e1770fbb9afd.js"
+  "/info_capstone_2020/precache-manifest.6e2b1a2a7ac9c6b188eeef3fdbae00b6.js"
 );
 
 self.addEventListener('message', (event) => {
